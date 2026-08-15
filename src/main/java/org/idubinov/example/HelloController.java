03d35bdce5b632ab4e328cbefed315c42bbf74a6
idubinov <dubinov.ivan@bk.ru>
2026-08-15 21:45:56 +0300

    feat: SpringMVC первое приложение

--- src/main/java/org/idubinov/example/HelloController.java
+++ src/main/java/org/idubinov/example/HelloController.java
@@ -1,4 +1,14 @@
 package org.idubinov.example;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
 }
