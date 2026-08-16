ee10db5d5c40db94b8e23c73e2d402995613c70c
idubinov <dubinov.ivan@bk.ru>
2026-08-16 11:16:56 +0300

    feat: SpringMVC Конфигурация через Java-код

--- src/main/java/org/idubinov/example/HelloController.java
+++ src/main/java/org/idubinov/example/HelloController.java
@@ -1,7 +1,6 @@
 package org.idubinov.example;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
---
+++ src/main/java/org/idubinov/example/config/mySpringMVCDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package org.idubinov.example.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class mySpringMVCDispatcherServletInitializer
+        extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?> @Nullable [] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?> @Nullable [] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
