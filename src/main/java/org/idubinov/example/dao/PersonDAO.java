c92b2e53cee67300eab462e401b1f2643c01c61d
idubinov <dubinov.ivan@bk.ru>
2026-08-17 15:30:52 +0300

    feat: Аннотация @ModelAttribute. HTML-формы (Thymeleaf)

--- src/main/java/org/idubinov/example/dao/PersonDAO.java
+++ src/main/java/org/idubinov/example/dao/PersonDAO.java
@@ -29,4 +29,9 @@ public class PersonDAO {
                 .findAny()
                 .orElse(null);
     }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
 }
